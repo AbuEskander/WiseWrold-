1fbb5dac1a4007ca51a2caa66028d54e80cc9272
Basil Ismail <basselawni1@gmail.com>
2024-05-31 01:55:22 +0300

    added CountryList and CountryItem and updated the Sidebar to work even better

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+/* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Home from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./Components/CityList";
+import CountryList from "./Components/CountryList";
+
+const BASE_URL = "http://localhost:3001/";
 // import PageNav from "./Components/PageNav";
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCity() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(BASE_URL + "cities");
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCity();
+  }, []);
   return (
     <>
       <BrowserRouter>
@@ -16,9 +40,15 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="*" element={<PageNotFound />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>Dashboard</p>} />
-            <Route path="cities" element={<p>List of Cites</p>} />
-            <Route path="countries" element={<p>List of Countries</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route path="countries" element={<CountryList cities={cities} />} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
           <Route path="/login" element={<Login />} />
